98c6e5c4e6a0eae0106b8df7cc96845d37046deb
Mayron Thoma Ceccon <mayron.ceccon@tecnorisk.com.br>
2019-10-11 17:27:32 -0300

    Ajustes diversos

--- app.py
+++ app.py
@@ -4,24 +4,6 @@ from threading import Lock
 import sqlalchemy as db
 from sqlalchemy.pool import StaticPool
 import json
-from sqlalchemy.ext.declarative import DeclarativeMeta
-
-class AlchemyEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj.__class__, DeclarativeMeta):
-            # an SQLAlchemy class
-            fields = {}
-            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
-                data = obj.__getattribute__(field)
-                try:
-                    json.dumps(data) # this will fail on non-encodable values, like other classes
-                    fields[field] = data
-                except TypeError:
-                    fields[field] = None
-            # a json-encodable dict
-            return fields
-
-        return json.JSONEncoder.default(self, obj)
 
 async_mode = None
 
@@ -30,8 +12,6 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
-
-
 
 engine = db.create_engine('sqlite:///socket.sqlite',
     connect_args={'check_same_thread': False},
